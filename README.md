02700e96693308cd00e390957b3f1f81064beadf
Siem Broeder <siembroeder@gmail.com>
2026-04-09 13:08:34 +0200

    Initial prototype commit

--- README.md
+++ README.md
@@ -2,6 +2,41 @@
 
 Overflight is a desktop agent that displays nearby aircrafts through icons that mirror their flight paths on the screen. 
 
+
+
+## Prototype / Development
+Not ready for use.
+Current functionality:
+- choose a location eg "Alkmaar", bounding box eg "small", monitor eg "eDP-1", and maximum amount of windows allowed eg "3".
+- spawns a duck at the corrent location relative to the chosen bounding box and screen.
+- move the window/duck when a new api call comes in.
+- close the window when the aircraft moves out of the bounding box.
+- creates a new window if a new aircraft moves into the bounding box and the max number of windows isn't reached.
+- runs indefinitely.
+
+
+
+Only Hyprland is supported for now, add these to your hyprland.conf file:
+- windowrule = float on, match:title ^qtApp_.*
+- windowrule = no_blur on, match:title ^qtApp_.*
+- windowrule = border_size 0, match:title ^qtApp_.*
+- windowrule = no_shadow on, match:title ^qtApp_.*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## Contributors
 
 This is a project by:
---
+++ customQtWindow.py
@@ -0,0 +1,85 @@
+
+
+import subprocess
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel
+
+
+
+
+
+        
+class MainWindow(QMainWindow):
+    def __init__(self, bbox:tuple, initPlaneCoords:tuple, icao24:str, callsign:str, showOnScreenName:str="eDP-1"):
+        super().__init__()
+        
+        self.icao24 = icao24
+        self.callsign = callsign
+        self.initPlaneCoords = initPlaneCoords
+        self.minLat, self.maxLat, self.minLong, self.maxLong = bbox
+        
+        screens = QApplication.screens()
+        
+        if showOnScreenName == 'all':
+            self.primaryScreen = QApplication.primaryScreen()
+            if self.primaryScreen is not None:
+                self.virtual_geom = self.primaryScreen.virtualGeometry()
+                self.Nxpixels = self.virtual_geom.getCoords()[2] + 1
+                self.Nypixels = self.virtual_geom.getCoords()[3] + 1            #print(f"{self.virtual_geom=}")
+        else:
+            for screen in screens:
+                if screen.name() == showOnScreenName:
+                    self.availableGeometry = screen.availableGeometry()
+                    self.Nxpixels, self.Nypixels = self.availableGeometry.width(), self.availableGeometry.height() 
+        
+        self.setWindowTitle(f"qtApp_{icao24}")
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+
+        label = QLabel(self)
+        pixmap = QPixmap('duck-left.gif')
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label)
+        self.resize(pixmap.width(), pixmap.height())  
+    
+    def coordsToPixels(self, planeCoords:tuple) -> tuple[int, int]: 
+        lon, lat = planeCoords
+
+        # normalize to 0-1 and multiply with number of available pixels
+        pixelx = int(((lon - self.minLong) / (self.maxLong - self.minLong) ) * self.Nxpixels)
+        pixely = int(((lat - self.minLat)  / (self.maxLat - self.minLat)   ) * self.Nypixels) # print(f"{[pixelx,pixely]=}")
+        
+        # since on hyprland y axis inverted:
+        pixely = abs(864 - pixely)              
+        
+        return pixelx, pixely
+        
+    def showEvent(self, a0) -> None: #a0 == event but qtwidgets complains
+        """Fires when window is first shown"""
+        super().showEvent(a0)             
+        QTimer.singleShot(200, lambda:self.moveToPlaneLoc(self.initPlaneCoords))
+                
+    def moveToPlaneLoc(self, planeCoords:tuple) -> None:
+        pixelx, pixely = self.coordsToPixels(planeCoords)
+        pixelx = int(pixelx - (self.width() / 2))       # update such that image is rendered at the center rather than top left
+        pixely = int(pixely - (self.height()/ 2))
+        
+        self.moveOnHyprland(pixelx, pixely)             
+        print(f"Moving {self.callsign} to {pixelx}, {pixely}")
+    
+    def moveOnHyprland(self, x, y):
+        subprocess.run(['hyprctl', 'dispatch', 'movewindowpixel', f'exact {x} {y},title:{self.windowTitle()}'], capture_output=True) # ^(qtApp)$
+
+
+
+
+
+
+
+def main():
+    print("Not supported to run as standalone .py file.")
+
+
+if __name__ == "__main__":
+    main()
