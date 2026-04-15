2d2d9b3a02477bbf456379eb340d83e31484ab34
Siem Broeder <siembroeder@gmail.com>
2026-04-15 17:18:14 +0200

    Feat: windows display callsign on hover (tooltip)

--- customQtWindow.py
+++ customQtWindow.py
@@ -19,6 +19,7 @@ class MainWindow(QMainWindow):
         self.initPlaneCoords = initPlaneCoords
         self.minLat, self.maxLat, self.minLong, self.maxLong = bbox
         
+        
         screens = QApplication.screens()
         
         if showOnScreenName == 'all':
@@ -33,6 +34,7 @@ class MainWindow(QMainWindow):
                     self.availableGeometry = screen.availableGeometry()
                     self.Nxpixels, self.Nypixels = self.availableGeometry.width(), self.availableGeometry.height() 
         
+        self.setToolTip(callsign)
         self.setWindowTitle(f"qtApp_{icao24}")
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
@@ -59,7 +61,7 @@ class MainWindow(QMainWindow):
         """Fires when window is first shown"""
         super().showEvent(a0)             
         QTimer.singleShot(200, lambda:self.moveToPlaneLoc(self.initPlaneCoords))
-                
+
     def moveToPlaneLoc(self, planeCoords:tuple) -> None:
         pixelx, pixely = self.coordsToPixels(planeCoords)
         pixelx = int(pixelx - (self.width() / 2))       # update such that image is rendered at the center rather than top left
