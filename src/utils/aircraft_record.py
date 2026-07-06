e5de0633d3ba3811ad556fb2a3eb1c4af1dfe688
Steef Broeder <steef@broeder.co.nl>
2026-07-06 15:29:37 +0200

    Move visual info from ac record to 8643 utils

--- src/utils/aircraft_record.py
+++ src/utils/aircraft_record.py
@@ -1,86 +1,13 @@
-
 from dataclasses import dataclass
-from PySide6.QtGui import QPixmap
 
 from opensky_api import StateVector
+
 from utils.icao8643_utils import Icao8643Entry
+
 
 @dataclass
 class AircraftRecord():
     """Small dataclass storing an aircrafts live openskyapi StateVector and metadata from icao8643 database entry"""
     state:StateVector
-    entry:Icao8643Entry
-
-    def get_visual_info(self) -> tuple[QPixmap, float]:
-        entry = self.entry
-        description = entry.aircraft_description.lower()
-        typecode = self.entry.typecode.upper()
-        
-        # Set defaults
-        factor = 1.0
-        image = QPixmap("assets/A321.png")
-
-        # Filter wtc
-        if entry.wtc == "L":
-            image = QPixmap("assets/C172.png")
-            factor = 0.5
-            
-        if entry.wtc == "M":
-            pass # default settings
-        
-        if entry.wtc == "H":
-            image = QPixmap("assets/B777.png")
-            factor = 1.1
-            
-        # Filter number of engines
-        if entry.engine_count == 3:
-            image = QPixmap("assets/md11.png")
-
-        # Filter specific typecodes
-        if typecode.startswith("B74"):
-            image = QPixmap("assets/B747.png")
-            factor = 1.2
-            
-        if typecode.startswith("B73"):
-            image = QPixmap("assets/B737.png")
-            
-        if typecode in ["a318", "a319", "a320", "a321"]:
-            image = QPixmap("assets/A321.png")
-            
-        if typecode == "A388":
-            image = QPixmap("assets/A380.png")
-            factor = 1.4
-            
-        if typecode == "C172":
-            image = QPixmap("assets/C172.png")
-            factor = 0.5
-            
-        # Filter descriptions
-        if description == "helicopter":
-            image = QPixmap("assets/helicopter.png")
-            factor = 0.7
-            
-        if description == "glider":
-            image = QPixmap("assets/glider.png")
-            factor = 0.6
-        
-        # Filter wtc
-        if entry.wtc == "L":
-            image = QPixmap("assets/C172.png")
-            factor = 0.5
-            
-        if entry.wtc == "M":
-            pass # default settings
-        
-        if entry.wtc == "H":
-            image = QPixmap("assets/B777.png")
-            factor = 1.1
-        
-        # Filter number of engines
-        if entry.engine_count == 3:
-            image = QPixmap("assets/md11.png")
-        
-        return (image, factor)
-
-        
+    entry:Icao8643Entry     
         
--- src/utils/icao8643_utils.py
+++ src/utils/icao8643_utils.py
@@ -3,6 +3,9 @@ import logging
 logger = logging.getLogger(__name__)
 from dataclasses import dataclass
 
+from PySide6.QtGui import QPixmap
+
+
 @dataclass
 class Icao8643Entry():
     model_full_name:str
@@ -35,3 +38,73 @@ class Icao8643Entry():
                                                            engine_type          = row["EngineType"])
                                                            for row in csv.DictReader(f)
                                                            }
+    
+    def get_visual_info(self) -> tuple[QPixmap, float]:
+        description = self.aircraft_description.lower()
+        typecode = self.typecode.upper()
+        
+        # Set defaults
+        factor = 1.0
+        image = QPixmap("assets/A321.png")
+
+        # Filter wtc
+        if self.wtc == "L":
+            image = QPixmap("assets/C172.png")
+            factor = 0.5
+            
+        if self.wtc == "M":
+            pass # default settings
+        
+        if self.wtc == "H":
+            image = QPixmap("assets/B777.png")
+            factor = 1.1
+            
+        # Filter number of engines
+        if self.engine_count == 3:
+            image = QPixmap("assets/md11.png")
+
+        # Filter specific typecodes
+        if typecode.startswith("B74"):
+            image = QPixmap("assets/B747.png")
+            factor = 1.2
+            
+        if typecode.startswith("B73"):
+            image = QPixmap("assets/B737.png")
+            
+        if typecode in ["a318", "a319", "a320", "a321"]:
+            image = QPixmap("assets/A321.png")
+            
+        if typecode == "A388":
+            image = QPixmap("assets/A380.png")
+            factor = 1.4
+            
+        if typecode == "C172":
+            image = QPixmap("assets/C172.png")
+            factor = 0.5
+            
+        # Filter descriptions
+        if description == "helicopter":
+            image = QPixmap("assets/helicopter.png")
+            factor = 0.7
+            
+        if description == "glider":
+            image = QPixmap("assets/glider.png")
+            factor = 0.6
+        
+        # Filter wtc
+        if self.wtc == "L":
+            image = QPixmap("assets/C172.png")
+            factor = 0.5
+            
+        if self.wtc == "M":
+            pass # default settings
+        
+        if self.wtc == "H":
+            image = QPixmap("assets/B777.png")
+            factor = 1.1
+        
+        # Filter number of engines
+        if self.engine_count == 3:
+            image = QPixmap("assets/md11.png")
+        
+        return (image, factor)
